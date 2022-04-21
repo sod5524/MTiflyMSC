f972010c541039e5aed08f96269caa0c63fa1c97
sod5524 <nzusb5@163.com>
2022-04-21 10:38:50 +0800

    sod5524

---
+++ Example/MTiflyMSC/MTViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTViewController.h
+//  MTiflyMSC
+//
+//  Created by apple on 04/21/2022.
+//  Copyright (c) 2022 apple. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MTViewController : UIViewController
+
+@end
---
+++ Example/MTiflyMSC/MTViewController.m
@@ -0,0 +1,29 @@
+//
+//  MTViewController.m
+//  MTiflyMSC
+//
+//  Created by apple on 04/21/2022.
+//  Copyright (c) 2022 apple. All rights reserved.
+//
+
+#import "MTViewController.h"
+
+@interface MTViewController ()
+
+@end
+
+@implementation MTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
